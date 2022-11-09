183fa661076e0c13b291793b3a215420e5341d2e
vladsfa <vladsfa.personal@gmail.com>
2022-11-10 01:59:26 +0200

    Add Minimization

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 import java.io.File;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         try {
             Dfa dfa = Dfa.ReadDfa(GetScanner("test.txt"));
+            Dfa.Minimize(dfa);
+            Print(dfa);
         }
         catch (Exception e)
         {
@@ -12,6 +16,20 @@ public class Main {
         }
     }
 
+    public static void Print(Dfa dfa)
+    {
+        System.out.println(dfa.alphabet.size());
+        System.out.println(dfa.states.size());
+        System.out.println(dfa.startState);
+        System.out.print(dfa.finalStates.size() + " ");
+        for (var st : dfa.finalStates)
+            System.out.print(st + " ");
+        System.out.println();
+        for (var st : dfa.transition.keySet())
+            for (var entry: dfa.transition.get(st).entrySet())
+                System.out.println(st + " " + entry.getKey() + " " + entry.getValue());
+
+    }
     public static Scanner GetScanner(String path) throws Exception {
         File file = new File(path);
         if (!file.exists())
